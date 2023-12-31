b441fa031e8146cc0fec9e3658dfb8e45c8aab7d
Biswajit Paul <134674632+biswajit-dev-009@users.noreply.github.com>
2023-12-31 12:30:22 +0530

    Update heading name

--- src/app/@analytics/mems/page.tsx
+++ src/app/@analytics/mems/page.tsx
@@ -6,7 +6,7 @@ const ChartsPage = async() => {
   
   return (
     <div className='bg-red-400 h-96 rounded-2xl w-1/2 p-10'>
-      <h4 className='text-2xl'>Charts</h4>
+      <h4 className='text-2xl'>Mems</h4>
       <div style={{ width: '280px' }}>
         <iframe
           allow='fullscreen'
